c36c2c89eff9ac5e3e6751bddfc03a5cddb57a8b
angelina.derman <angelina.derman@gmail.com>
2021-03-04 16:11:50 +0300

    feat: add build

---
+++ precache-manifest.a1ab649573b953ccc04ee6bb3da3855e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94e1705dd39dd925e111962610ce6428",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6815f15795eea5e3c2d1",
+    "url": "/static/css/main.f39d5fc6.chunk.css"
+  },
+  {
+    "revision": "9dfeb573889a09072c43",
+    "url": "/static/js/2.e7a6089d.chunk.js"
+  },
+  {
+    "revision": "6815f15795eea5e3c2d1",
+    "url": "/static/js/main.b47d275a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
